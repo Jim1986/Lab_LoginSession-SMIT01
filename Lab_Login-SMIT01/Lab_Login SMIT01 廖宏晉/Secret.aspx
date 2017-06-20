b61882b021f9535c0ff07365b700d357db1d04ec
Jim1986 <Liaohongjin@gmail.com>
2017-06-20 18:52:52 +0800

    Add files via upload

---
+++ Lab_Login-SMIT01/Lab_Login SMIT01 廖宏晉/Secret.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Secret.aspx.cs" Inherits="Secret" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <p>
+    Content for member only.</p>
+<p>
+    <br />
+    <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+</p>
+<p>
+</p>
+</asp:Content>
+
---
+++ Lab_Login-SMIT01/Lab_Login SMIT01 廖宏晉/Secret.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Secret : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(Session["userName"].ToString())) {
+            Session["url"] = "Secret.aspx";
+            Response.Redirect("Login.aspx");
+            
+        }
+        
+        Literal1.Text = "Hello" + " " + Session["userName"];
+
+    }
+}
